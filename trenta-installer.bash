c6234630313bfd06e63575801e5b21ba06e4ff3f
Kenny Stier <19kestier@users.noreply.github.com>
2014-11-16 18:37:27 -0500

    Update to SA2

--- trenta-installer.bash
+++ trenta-installer.bash
@@ -5,21 +5,19 @@
 # This program is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later version.
-# 
-# wget -q -O - https://trentaos.org/installer-files/trenta-installer.bash | bash
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-echo "Welcome to the Trenta OS Installer version 0.1!"
+echo "Welcome to the Trenta OS Installer version 0.2!"
 # Check if root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 # Check if running Ubuntu 14.04.1 LTS
- if [[ $(lsb_release -sd) != "Ubuntu 14.04.1 LTS" ]];then
+ if [[ $(lsb_release -si) != "Ubuntu" ]];then
     echo "Error: It appears you are not running Ubuntu 14.04.1 LTS."
     exit 1
  fi
@@ -43,7 +41,9 @@ else
 fi
 echo "NOTE: If it asks you what display manager to use, be sure to select GDM!"
 zenity --info --icon-name=trenta --text="Check on the script every once in a while. If it asks you what display manager to use, be sure to use your keyboard to select GDM!"
+echo "Rainier Plank Theme will have an error: it's perfectly normal!"
 # Start Questioning
+# What Applications to Remove
 echo ""
 echo "This script will remove the following applications:"
 echo " Firefox"
@@ -121,6 +121,7 @@ fi
 
 fi
 echo ""
+# Question Media Codecs
 read -r -p "Install media codecs? [Y/n] " response
 response=${response,,}
 if [[ $response =~ ^(yes|y| ) ]]
@@ -167,7 +168,7 @@ fi
 
 echo ""
 echo " To Install:"
-echo "  Docky"
+echo "  Plank"
 echo "  Gnome Panel"
 echo "  Rainier Theme Pack"
 echo "  VLC"
@@ -179,14 +180,15 @@ echo "  $CODECS"
 echo ""
 read -p "Do you want to continue (Y/n)? "
 [ "$(echo $REPLY | tr [:upper:] [:lower:])" == "y" ] || exit
-# Begin
+# Install GDM
 echo "Installing GDM"
 apt-get -yqq update
 apt-get -yqq install gdm
 apt-get -yqq purge lightdm
 if [[ $removeALL = true ]];then
 echo "Removing the listed applications..."
-apt-get -yqq purge firefox empathy unity thunderbird totem rhythmbox
+# Remove apps if All were selected
+apt-get -yqq purge firefox empathy unity thunderbird totem rhythmbox docky
 fi
 if [[ $removeALL = false ]]; then
 if [[ $removeFIREFOX = true ]]; then
@@ -210,9 +212,11 @@ fi
 if [[ $installCODECS = true ]]; then
 media=libavcodec-extra\ openjdk-7-jre\ icedtea-7-plugin\ flashplugin-installer\ unace\ unrar\ zip\ unzip\ p7zip-full\ p7zip-rar\ sharutils\ rar\ uudeview\ mpack\ arj\ cabextract\ file-roller\ libxine1-ffmpeg\ mencoder\ flac\ faac\ faad\ sox\ ffmpeg2theora\ libmpeg2-4\ uudeview\ libmpeg3-1\ mpeg3-utils\ mpegdemux\ liba52-dev\ mpeg2dec\ vorbis-tools\ id3v2\ mpg321\ mpg123\ libflac++6\ totem-mozilla\ icedax\ lame\ libmad0\ libjpeg-progs\ libdvdcss2\ libdvdread4\ libdvdnav4\ libswscale-extra-2\ ubuntu-restricted-extras\ gstreamer0.10-ffmpeg\ pepperflashplugin-nonfree
 fi
+# Remove if App Removal was Selective
 echo "Removing selected applications..."
-apt-get -yqq purge laptop-mode-tools $Firefox $Empathy $Unity $Thunderbird $Totem $Rhythmbox
+apt-get -yqq purge laptop-mode-tools docky $Firefox $Empathy $Unity $Thunderbird $Totem $Rhythmbox
 fi
+# Add Repos
 echo "Adding Repositories..."
 add-apt-repository -y ppa:trentaos-team/ppa
 add-apt-repository -y ppa:trentaos-team/rainier
@@ -231,21 +235,30 @@ wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list
 add-apt-repository -y main
 add-apt-repository -y universe
 echo "Updating Software Sources..."
+# Update Repos
 apt-get -yqq update
+# Updating Software
+echo "Updating Ubuntu..."
+apt-get -yqq dist-upgrade
 echo "Installing Software..."
-apt-get -yqq install fonts-roboto synapse indicator-synapse docky gnome-panel rainier-gtk-theme rainier-icon-theme rainier-docky-theme vlc musique undistract-me playonlinux midori preload process-monitor trenta-os-bootscreen y-ppa-manager gtk2-engines-murrine gtk2-engines-pixbuf tlp tlp-rdw slingscold
+# Start Install of Trenta Software
+apt-get -yqq install fonts-roboto synapse indicator-synapse plank gnome-panel rainier-gtk-theme rainier-icon-theme rainier-plank-theme vlc musique undistract-me playonlinux midori preload process-monitor trenta-os-bootscreen y-ppa-manager gtk2-engines-murrine gtk2-engines-pixbuf tlp tlp-rdw slingscold indicator-applet-appmenu compiz-plugins compizconfig-settings-manager
+# Install Codecs
 echo "Installing Codecs..."
-notify-send --urgency=critical --app-name=gnome-terminal --icon=trenta Trenta\ OS Codecs\ installing...
 apt-get -yqq install $media
 echo "Configuring Trenta..."
+# Config Codecs
 if [[ $installCODECS = true ]]; then
 	update-pepperflashplugin-nonfree --install
 	bash /usr/share/doc/libdvdread4/install-css.sh
 	tlp start
 fi
+# Disable Apportt
 echo "enabled=0" > /etc/default/apport
 service apport stop
+# Enable Apt Progress Bar
 echo 'Dpkg::Progress-Fancy "1";' > /etc/apt/apt.conf.d/99progressbar
+# Set Fonts
 gsettings set org.gnome.desktop.interface document-font-name 'Roboto 11'
 gsettings set org.gnome.desktop.interface font-name 'Roboto 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Droid Sans Mono 11'
@@ -255,14 +268,10 @@ gsettings set org.gnome.desktop.interface gtk-theme "Rainier"
 gsettings set org.gnome.desktop.interface icon-theme 'Rainier'
 gsettings set org.gnome.desktop.wm.preferences theme "Rainier"
 # Configure Docky
-pkill docky
-gconftool-2 -s -t string /apps/docky-2/Docky/Services/ThemeService/Theme Rainier
-gconftool-2 -s -t string /apps/docky-2/Docky/Interface/DockPreferences/Dock1/Autohide Intellihide
-gconftool-2 --type list --list-type string --set /apps/docky-2/Docky/Interface/DockPreferences/Dock1/Launchers '[file:///usr/share/applications/midori.desktop,file:///usr/share/applications/nautilus.desktop,/usr/share/applications/vlc.desktop,file:///usr/share/applications/musique.desktop,file:///usr/share/applications/libreoffice-startcenter.desktop,file:///usr/share/applications/ubuntu-software-center.desktop,file:///usr/share/applications/gnome-terminal.desktop,file:///usr/share/applications/unity-control-center.desktop]' 
-gconftool-2 --type list --list-type string --set /apps/docky-2/Docky/Interface/DockPreferences/Dock1/Plugins '[Mounter,Trash]'
-gconftool-2 -s -t boolean /apps/docky-2/Docky/Interface/DockPreferences/Dock1/ThreeDimensional true
-gconftool-2 --type Boolean --set /apps/docky-2/Docky/Items/DockyItem/ShowDockyItem False
-rm /usr/share/icons/slingscold.png
+
+# Remove stolen Slingscold icon
+rm -f /usr/share/icons/slingscold.png
+# Edit Slingscold .desktop file
 echo "[Desktop Entry]" > /usr/share/applications/slingscold.desktop
 echo "Name=Slingscold" >> /usr/share/applications/slingscold.desktop
 echo "Comment=Application" >> /usr/share/applications/slingscold.desktop
@@ -272,19 +281,29 @@ echo "Type=Application" >> /usr/share/applications/slingscold.desktop
 echo "Categories=GNOME;GTK;Utility;" >> /usr/share/applications/slingscold.desktop
 echo "StartupNotify=false" >> /usr/share/applications/slingscold.desktop
 echo "Comment[en_US.UTF-8]=Application Launcher" >> /usr/share/applications/slingscold.desktop
+# Create Temporary Directory to Download Files Into
 mkdir -p ~/.trenta/installer
 cd ~/.trenta/installer
-wget -q http://trentaos.org/installer-files/trenta-wallpaper.png
-wget -q http://trentaos.org/installer-files/ubuntu-wallpapers.xml
-wget -q http://trentaos.org/installer-files/UbuntuLogo.png
-wget -q http://trentaos.org/installer-files/searchingthedashlegalnotice.html
-wget -q http://trentaos.org/installer-files/appearance.ui
-wget -q http://trentaos.org/installer-files/process-monitor.desktop
-wget -q http://trentaos.org/installer-files/synapse.desktop
-wget -q http://trentaos.org/installer-files/bear.png
-wget -q http://trentaos.org/installer-files/os-release
-wget -q http://trentaos.org/installer-files/lsb-release
-wget -q http://trentaos.org/installer-files/docky
+# Download files from Trenta's Servers
+wget -q http://trentaos.org/installer-files/SA2/trenta-wallpaper.png
+wget -q http://trentaos.org/installer-files/SA2/ubuntu-wallpapers.xml
+wget -q http://trentaos.org/installer-files/SA2/UbuntuLogo.png
+wget -q http://trentaos.org/installer-files/SA2/searchingthedashlegalnotice.html
+wget -q http://trentaos.org/installer-files/SA2/appearance.ui
+wget -q http://trentaos.org/installer-files/SA2/process-monitor.desktop
+wget -q http://trentaos.org/installer-files/SA2/synapse.desktop
+wget -q http://trentaos.org/installer-files/SA2/bear.png
+wget -q http://trentaos.org/installer-files/SA2/os-release
+wget -q http://trentaos.org/installer-files/SA2/lsb-release
+wget -q http://trentaos.org/installer-files/SA2/gnome-terminal.dockitem
+wget -q http://trentaos.org/installer-files/SA2/libreoffice-startcenter.dockitem
+wget -q http://trentaos.org/installer-files/SA2/midori.dockitem
+wget -q http://trentaos.org/installer-files/SA2/musique.dockitem
+wget -q http://trentaos.org/installer-files/SA2/nautilus.dockitem
+wget -q http://trentaos.org/installer-files/SA2/ubuntu-software-center.dockitem
+wget -q http://trentaos.org/installer-files/SA2/unity-control-center.dockitem
+wget -q http://trentaos.org/installer-files/SA2/vlc.dockitem
+# Move Downloaded Files into Place
 mv -f trenta-wallpaper.png /usr/share/backgrounds
 mv -f ubuntu-wallpapers.xml /usr/share/gnome-background-properties/
 mv -f UbuntuLogo.png /usr/share/unity-control-center/ui
@@ -294,12 +313,7 @@ mv -f os-release /etc
 mv -f lsb-release /etc
 mv -f bear.png /usr/share/icons/hicolor/96x96/apps
 gtk-update-icon-cache -f /usr/share/icons/hicolor/
-mv ~/.trenta/installer/process-monitor.desktop /etc/xdg/autostart
-mv ~/.trenta/installer/synapse.desktop /etc/xdg/autostart
-mv ~/.trenta/installer/docky /usr/share/docky/
-# Updating Software
-echo "Updating Ubuntu..."
-apt-get -yqq dist-upgrade
+mv -f ~/.trenta/installer/synapse.desktop /etc/xdg/autostart
 # Cleaning Up
 echo "Cleaning Up..."
 apt-get -f install
@@ -307,6 +321,7 @@ apt-get autoremove
 apt-get -y autoclean
 apt-get -y clean
 # Time for part two
+plank &
 echo "Time for part two! Your system will restart shortly."
 notify-send --urgency=critical --app-name=gnome-terminal --icon=trenta Trenta\ OS Time\ for\ part\ two!
 zenity --info --icon-name=trenta --text="Please save your work and hit OK so that we can restart your system. When it boots back up, click the gear icon and select Gnome Classic (Compiz). Then login. Once you get logged in, run wget http://trentaos.org/installer-files/trenta-installer2.bash && bash trenta-installer2.bash"
